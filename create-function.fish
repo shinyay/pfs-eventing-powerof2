b768b0dab209c6e3539ac6ee747263a5db7143ed
shinyay <shinya.com@gmail.com>
2018-12-29 21:44:00 +0900

    Modify option for artifact
    Add specific artifact name

--- create-function.fish
+++ create-function.fish
@@ -5,5 +5,6 @@ set -x REGISTRY_USER (gcloud config get-value core/project)
 
 pfs function create powerof2 \
              --git-repo https://github.com/shinyay/pfs-eventing-powerof2.git \
+             --artifact powerof2.js \
              --image $REGISTRY/$REGISTRY_USER/powerof2 \
              --verbose
